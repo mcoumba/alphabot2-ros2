d051b836f8a9b601824e152101acbd69ca9372cb
mcoumba <mbaye.coumba2706@gmail.com>
2026-06-15 15:49:52 +0200

    maj follower

--- alphabot2/alphabot2_follower/alphabot2_follower/follower.py
+++ alphabot2/alphabot2_follower/alphabot2_follower/follower.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+##################################################################_Importations_###################################################################
 
 import rclpy
 from rclpy.node import Node
@@ -6,99 +6,71 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 import math
 
+#########################################_Classes_#################################################################################################
+# Cette classe est un noeud ROS2 qui suit la position publiée par le leader
+class Follower(Node):
 
-class LeaderFollower(Node):
+###################################_Methodes_####################################################################################################    
     def __init__(self):
-        super().__init__('leader_follower_node')
+        super().__init__('follower_node')
 
-        self.leader_pose = None
-        self.follower_pose = None
-
-        self.sub_leader = self.create_subscription(
-            Odometry,
-            '/barista_1/odom',
-            self.leader_callback,
-            10
-        )
-
-        self.sub_follower = self.create_subscription(
-            Odometry,
-            '/barista_2/odom',
-            self.follower_callback,
-            10
-        )
-
-        self.pub_cmd = self.create_publisher(
-            Twist,
-            '/barista_2/cmd_vel',
-            10
-        )
-
+        #follower subscribe à la position du leader 
+        self.sub_leader = self.create_subscription(Odometry,'/position_leader',self.leader_callback,10)
+        self.pub_cmd = self.create_publisher(Twist,'/cmd_vel',10)
         self.timer = self.create_timer(0.1, self.control_loop)
 
-        self.distance_desiree = 1.0
-        self.k_v = 0.5
-        self.k_w = 1.5
+        self.dt= 0.1
+        #Pose estimée du follower, On suppose qu'il démarre derrière le leader
+        self.x = -1.0
+        self.y = 0.0
+        self.theta = 0.0
+
+        #pose du leader
+        self.leader_x = 0.0
+        self.leader_y = 0.0
+        self.leader_theta = 0.0
+        self.leader_pose_received = False
+
+        #distance entre les deux robots
+        self.desired_robot_distance = 1.0
+
+        self.timer = self.create_timer(self.dt, self.timer_callback)
+
+        self.get_logger().info("Le noeud follower est démarré !!")
 
     def leader_callback(self, msg):
-        self.leader_pose = msg.pose.pose
+        self.leader_x=msg.x
+        self.leader_y=msg.y
+        self.leader_theta=msg.theta
+        self.leader_pose_received = True
 
-    def follower_callback(self, msg):
-        self.follower_pose = msg.pose.pose
+    def timer_callback(self):
+        cmd_follower=Twist()
 
-    def get_yaw(self, orientation):
-        qx = orientation.x
-        qy = orientation.y
-        qz = orientation.z
-        qw = orientation.w
-
-        siny_cosp = 2 * (qw * qz + qx * qy)
-        cosy_cosp = 1 - 2 * (qy * qy + qz * qz)
-
-        return math.atan2(siny_cosp, cosy_cosp)
-
-    def normalize_angle(self, angle):
-        while angle > math.pi:
-            angle -= 2 * math.pi
-        while angle < -math.pi:
-            angle += 2 * math.pi
-        return angle
-
-    def control_loop(self):
-        if self.leader_pose is None or self.follower_pose is None:
+        if not self.leader_pose_received:
+            self.cmd_vel_pub.publish(cmd_follower)
+            self.get_logger().info("En attente de la position du leader...")
             return
-
-        xL = self.leader_pose.position.x
-        yL = self.leader_pose.position.y
-
-        xF = self.follower_pose.position.x
-        yF = self.follower_pose.position.y
-        thetaF = self.get_yaw(self.follower_pose.orientation)
-
-        dx = xL - xF
-        dy = yL - yF
-
+        
+        #calcul de la distance et de l'angle entre le follower et le leader
+        dx = self.leader_x - self.x
+        dy = self.leader_y - self.y
         distance = math.sqrt(dx**2 + dy**2)
         angle_to_leader = math.atan2(dy, dx)
 
-        erreur_angle = self.normalize_angle(angle_to_leader - thetaF)
-        erreur_distance = distance - self.distance_desiree
-
-        cmd = Twist()
-
-        if distance > self.distance_desiree:
-            cmd.linear.x = self.k_v * erreur_distance
-            cmd.angular.z = self.k_w * erreur_angle
+        if distance > self.desired_robot_distance:
+            cmd_follower.linear.x = 0.5
         else:
-            cmd.linear.x = 0.0
-            cmd.angular.z = 0.0
+            cmd_follower.linear.x = 0.0
 
-        self.pub_cmd.publish(cmd)
+        self.pub_cmd.publish(cmd_follower)
+
+        self.get_logger().info( "Distance leader/follower = " + str(round(distance, 2)) +"  cmd linear.x = " + str(cmd_follower.linear.x ))
 
 
 def main(args=None):
     rclpy.init(args=args)
-    node = LeaderFollower()
+    node = Follower()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
@@ -106,3 +78,4 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+    
